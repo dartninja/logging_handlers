1c4812d2b2779718f1acb9324145f34b15af6ba4
Matthew Barbour <sanmadjack@users.noreply.github.com>
2018-08-13 09:33:11 -0500

    Update browser_logging_handlers.dart

--- lib/browser_logging_handlers.dart
+++ lib/browser_logging_handlers.dart
@@ -12,17 +12,17 @@ export 'logging_handlers_shared.dart';
  * iteration of the event loop, so it lives in a Timer.run.
  * Startup items won't be logged.
  */
-void attachXLoggerUi([bool addPrintHandler=true]) {
-  Timer.run(() {
-    var loggerComponents = querySelectorAll("div[is=x-loggerui]");
-    print(loggerComponents);
-    var listener = Logger.root.onRecord.asBroadcastStream();
-    loggerComponents.forEach((component) {
-    		var loggerHandler = component.xtag;
-    		listener.listen(loggerHandler);
-    	});
-    
-    //optionally attach a default print handler
-    if (addPrintHandler) listener.listen(new LogPrintHandler());
-  });
-}
+//void attachXLoggerUi([bool addPrintHandler=true]) {
+//  Timer.run(() {
+//    var loggerComponents = querySelectorAll("div[is=x-loggerui]");
+//    print(loggerComponents);
+//    var listener = Logger.root.onRecord.asBroadcastStream();
+//    loggerComponents.forEach((component) {
+//    		var loggerHandler = component.xtag;
+//    		listener.listen(loggerHandler);
+//    	});
+//    
+//    //optionally attach a default print handler
+//    if (addPrintHandler) listener.listen(new LogPrintHandler());
+//  });
+//}
